f265d4a53d28b5017e00b678bf07d8bd9f63dc34
Michael Cheung <michaelcheung.cheme@gmail.com>
2021-09-15 11:31:00 -0600

    added yarn components(semantic axios react router dom) navbar and basic homepage)

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar';
+import { Container } from 'semantic-ui-react';
+import { Route, Switch } from 'react-router';
+import Home from './Components/Home.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <NavBar/>
+   <Container>
+     <Switch>
+       <Route exact path="/" component={Home} />
+       <Route component={() => <p>react router 404 path not found</p>} />
+      </Switch>
+   </Container>
+   </>
   );
 }
 
