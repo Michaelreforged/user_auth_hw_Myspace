4ed63c144364d07e25eb2e759f53f51dcf03712a
Michael Cheung <michaelcheung.cheme@gmail.com>
2021-09-26 20:45:19 -0600

    trying to style

--- client/src/Pages/UserPages/ViewUserpage.js
+++ client/src/Pages/UserPages/ViewUserpage.js
@@ -1,6 +1,6 @@
 import React, { useContext} from "react";
 import { Link } from "react-router-dom";
-import { Button } from "semantic-ui-react";
+import { Button, Card } from "semantic-ui-react";
 import { AuthContext } from "../../Providers/AuthProvider";
 
 export default function ViewUserPage(props) {
@@ -9,15 +9,15 @@ export default function ViewUserPage(props) {
   console.log(currentUser)
   const renderUser = () => {
     return (
-      <>
-      <h1>{currentUser.email}</h1>
-      <h1>{currentUser.name}</h1>
+      <Card>
+      <Card.Header>{currentUser.email}</Card.Header>
+      <Card.Content>{currentUser.name}</Card.Content>
       <Link to={`/user/edit`}>
             <Button color="green">
               Edit
             </Button>
       </Link>
-      </>
+      </Card>
     )
   }
 
